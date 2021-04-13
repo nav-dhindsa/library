5f9924f0143c20795f0b22c006863890db3bbde0
Navdeep Dhindsa <ndhindsa94@gmail.com>
2021-04-12 21:56:27 -0500

    Add delete book path and proper date for publication_date

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Book extends Model
 {
     protected $guarded = [];
+
+    protected $dates = ['publication_date'];
+
+    public function setPublicationDateAttribute($publication_date)
+    {
+        $this->attributes['publication_date'] = Carbon::parse($publication_date);
+    }
+
+    public function path()
+    {
+        return '/books' . $this->id;
+    }
 }
