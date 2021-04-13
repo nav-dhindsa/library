05ffd951115808d540ee6c051edd81044b5f3215
Navdeep Dhindsa <ndhindsa94@gmail.com>
2021-04-13 02:40:06 -0500

    Modify register layout and add some js and css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date of birth') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth">
+
+                                @error('date_of_birth')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
