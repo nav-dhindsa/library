80274fefadb4319dc618616d8d61496377ac0a55
Navdeep Dhindsa <ndhindsa94@gmail.com>
2021-04-13 02:39:34 -0500

    Create user and book seeders, Add methods to Registration and book controllers

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'date_of_birth' => $data['date_of_birth'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -8,35 +8,40 @@ use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
-    public function store() 
-    {
-      $book = Book::create($this->validateRequest());
-      return redirect($book->path());
-    }
+  public function create()
+  {
+    return view('books.create');
+  }
 
-    public function update(Book $book)
-    {
-      $book->update($this->validateRequest());
-      return redirect($book->path());
+  public function store() 
+  {
+    $book = Book::create($this->validateRequest());
+    return redirect($book->path());
+  }
 
-    }
+  public function update(Book $book)
+  {
+    $book->update($this->validateRequest());
+    return redirect($book->path());
 
-    public function destroy(Book $book)
-    {
-      $book->delete();
+  }
 
-      return redirect('/books');
-    }
+  public function destroy(Book $book)
+  {
+    $book->delete();
 
-    /**
-     * @return mixed
-     */
-    protected function validateRequest()
-    {
-      return request()->validate([
-        'title' => 'required',
-        'isbn' => 'required',
-        'publication_date' => 'required',
-      ]);
-    }
+    return redirect('/books');
+  }
+
+  /**
+   * @return mixed
+   */
+  protected function validateRequest()
+  {
+    return request()->validate([
+      'title' => 'required',
+      'isbn' => 'required',
+      'publication_date' => 'required',
+    ]);
+  }
 }
